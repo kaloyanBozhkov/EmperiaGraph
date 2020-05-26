66ceb455512019b0cfea3cca57817db68a22d3c0
Kaloyan Bozhkov <koko@lot.to>
2020-05-26 21:20:14 +0300

    fixed connection credentials

--- server/helpers/setupConnection.js
+++ server/helpers/setupConnection.js
@@ -1,14 +1,10 @@
 const mysql = require('mysql')
-const REACT_APP_USERNAME = process.env.mysql_username
-const REACT_APP_PASSWORD = process.env.mysql_password
-const REACT_APP_HOST = process.env.mysql_host
-const REACT_APP_DATABASE = process.env.mysql_database
 
 const connection = mysql.createPool({
-  host: REACT_APP_HOST,
-  user: REACT_APP_USERNAME,
-  password: REACT_APP_PASSWORD,
-  database: REACT_APP_DATABASE,
+  host: process.env.REACT_APP_HOST,
+  user: process.env.REACT_APP_USERNAME,
+  password: process.env.REACT_APP_PASSWORD,
+  database: process.env.REACT_APP_DATABASE,
 })
 
 module.exports = connection
