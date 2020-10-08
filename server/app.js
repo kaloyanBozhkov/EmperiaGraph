89aa0a3d6b09040330a8eea1d292bfeda50a195b
Koko <koko@Kokos-MBP.lan>
2020-10-08 19:41:52 +0200

    added try catches to API endpoints

--- server/app.js
+++ server/app.js
@@ -14,62 +14,73 @@ app.use(express.json())
 
 // get friends
 app.get('/emperia/friends', (req, res) => {
-  connection.query(
-    'SELECT * FROM ' + process.env.REACT_APP_EMPERIA_GRAPH_TABLE_FRIENDS,
-    (err, results, fields) => {
-      if (err) {
-        res.json(err)
-      } else {
-        res.json({
-          results,
-          fields,
-        })
+  try {
+    connection.query(
+      'SELECT * FROM ' + process.env.REACT_APP_EMPERIA_GRAPH_TABLE_FRIENDS,
+      (err, results, fields) => {
+        if (err) {
+          res.json(err)
+        } else {
+          res.json({
+            results,
+            fields,
+          })
+        }
       }
-    }
-  )
+    )
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 })
 
 
 // get connections
 app.get('/emperia/connections', (req, res) => {
-  connection.query(
-    'SELECT * FROM ' + process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS,
-    (err, results, fields) => {
-      if (err) {
-        res.json(err)
-      } else {
-        res.json({
-          results,
-          fields,
-        })
+  try {
+    connection.query(
+      'SELECT * FROM ' + process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS,
+      (err, results, fields) => {
+        if (err) {
+          res.json(err)
+        } else {
+          res.json({
+            results,
+            fields,
+          })
+        }
       }
-    }
-  )
+    )
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 })
 
 // get friends and connections
 app.get('/emperia/data', (req, res) => {
+  try {
+    connection.query(
+      `SELECT * FROM ${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_FRIENDS};
+         SELECT * FROM ${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS}`,
+      (err, results, fields) => {
+        if (err) {
+          res.json(err)
+        } else {
 
-  connection.query(
-    `SELECT * FROM ${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_FRIENDS};
-       SELECT * FROM ${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS}`,
-    (err, results, fields) => {
-      if (err) {
-        res.json(err)
-      } else {
+          const [friends, connections] = results
+          const formattedConnections = formatEdges(connections, friends)
+          const formattedFriends = formatFriends(friends, formattedConnections)
+          const connectionsFormatted = purifyConnections(formattedFriends)
 
-        const [friends, connections] = results
-        const formattedConnections = formatEdges(connections, friends)
-        const formattedFriends = formatFriends(friends, formattedConnections)
-        const connectionsFormatted = purifyConnections(formattedFriends)
-
-        res.json({
-          connections: connectionsFormatted,
-          friends: formattedFriends
-        })
+          res.json({
+            connections: connectionsFormatted,
+            friends: formattedFriends
+          })
+        }
       }
-    }
-  )
+    )
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 })
 
 app.get('/emperia', (req, res) =>
@@ -78,135 +89,151 @@ app.get('/emperia', (req, res) =>
 
 // add friend
 app.post('/emperia/friend', (req, res) => {
+  try {
 
-  // get friend data
-  const friend = req.body
+    // get friend data
+    const friend = req.body
 
-  if (!friend.firstName || !friend.lastName || !friend.totalFriends || !friend.sex) {
-    res.json({ error: 'The provided friend object is invalid, make sure it has firstName, lastName, totalFriends and sex.' })
-  }
+    if (!friend.firstName || !friend.lastName || !friend.totalFriends || !friend.sex) {
+      res.json({ error: 'The provided friend object is invalid, make sure it has firstName, lastName, totalFriends and sex.' })
+    }
 
-  const sqlQuery = `INSERT INTO 
+    const sqlQuery = `INSERT INTO 
   \`${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_FRIENDS}\`
   (\`firstName\`,\`lastName\`,\`totalFriends\`,\`sex\`)
   VALUES ('${friend.firstName}', '${friend.lastName}', '${friend.totalFriends}', '${friend.sex}')`
 
-  // create insertion query
-  connection.query(
-    sqlQuery,
-    (err, results, fields) => {
-      if (err) {
-        res.json(err)
-      } else {
-        res.json({
-          results,
-          fields,
-        })
+    // create insertion query
+    connection.query(
+      sqlQuery,
+      (err, results, fields) => {
+        if (err) {
+          res.json(err)
+        } else {
+          res.json({
+            results,
+            fields,
+          })
+        }
       }
-    }
-  )
+    )
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 })
 
 // add connection
 app.post('/emperia/connections', (req, res) => {
+  try {
 
-  // get connections data
-  const { connections = [] } = req.body
+    // get connections data
+    const { connections = [] } = req.body
 
-  if (!connections.length) {
-    res.json({ error: 'Array of connections must be provided' })
-  }
+    if (!connections.length) {
+      res.json({ error: 'Array of connections must be provided' })
+    }
 
-  if (connections.filter(({ source, target }) => !source || !target).length > 0) {
-    res.json({ error: 'Array of connections must be made of { source, target } items.' })
-  }
+    if (connections.filter(({ source, target }) => !source || !target).length > 0) {
+      res.json({ error: 'Array of connections must be made of { source, target } items.' })
+    }
 
-  const sqlQuery = `INSERT INTO 
+    const sqlQuery = `INSERT INTO 
   \`${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS}\`
   (\`source\`,\`target\`)
   VALUES ${connections.map(({ source, target }) => (`('${source}','${target}')`)).join(' ')}`
 
-  // create insertion query
-  connection.query(
-    sqlQuery,
-    (err, results, fields) => {
-      if (err) {
-        res.json(err)
-      } else {
-        res.json({
-          results,
-          fields,
-        })
+    // create insertion query
+    connection.query(
+      sqlQuery,
+      (err, results, fields) => {
+        if (err) {
+          res.json(err)
+        } else {
+          res.json({
+            results,
+            fields,
+          })
+        }
       }
-    }
-  )
+    )
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 })
 
 // delete friend
 app.delete('/emperia/friend', (req, res) => {
+  try {
 
-  const { id: friendId = null } = req.body
+    const { id: friendId = null } = req.body
 
-  if (!friendId || parseInt(friendId) !== friendId) {
-    res.json({ error: 'No friend Id provided.' })
-  }
-
-  const sqlQuery = `DELETE FROM
-  \`${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS}\`
-  WHERE \`source\` = ${friendId} OR \`target\` = ${friendId};
-  
-  DELETE FROM 
-  \`${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_FRIENDS}\`
-  WHERE \`id\` = ${friendId};`
-
-  // create insertion query
-  connection.query(
-    sqlQuery,
-    (err, results, fields) => {
-      if (err) {
-        res.json(err)
-      } else {
-        res.json({
-          results,
-          fields,
-        })
-      }
+    if (!friendId || parseInt(friendId) !== friendId) {
+      res.json({ error: 'No friend Id provided.' })
     }
-  )
+
+    const sqlQuery = `DELETE FROM
+    \`${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS}\`
+    WHERE \`source\` = ${friendId} OR \`target\` = ${friendId};
+    
+    DELETE FROM 
+    \`${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_FRIENDS}\`
+    WHERE \`id\` = ${friendId};`
+
+    // create insertion query
+    connection.query(
+      sqlQuery,
+      (err, results, fields) => {
+        if (err) {
+          res.json(err)
+        } else {
+          res.json({
+            results,
+            fields,
+          })
+        }
+      }
+    )
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 })
 
 // delete connection
 app.delete('/emperia/connections', (req, res) => {
+  try {
 
-  const { connections = [] } = req.body
+    const { connections = [] } = req.body
 
-  if (!connections.length) {
-    res.json({ error: 'Array of connections must be provided' })
-  }
+    if (!connections.length) {
+      res.json({ error: 'Array of connections must be provided' })
+    }
 
-  if (connections.filter(({ source, target }) => !source || !target).length > 0) {
-    res.json({ error: 'Array of connections must be made of { source, target } items.' })
-  }
+    if (connections.filter(({ source, target }) => !source || !target).length > 0) {
+      res.json({ error: 'Array of connections must be made of { source, target } items.' })
+    }
 
-  const sqlQuery = `DELETE FROM
+    const sqlQuery = `DELETE FROM
   \`${process.env.REACT_APP_EMPERIA_GRAPH_TABLE_CONNECTIONS}\`
   WHERE \`source\` IN (${connections.map(({ source }) => source).join(',')}) 
   AND \`target\` IN (${connections.map(({ target }) => target).join(',')})`
 
-  // create insertion query
-  connection.query(
-    sqlQuery,
-    (err, results, fields) => {
-      if (err) {
-        res.json(err)
-      } else {
-        res.json({
-          results,
-          fields,
-        })
+    // create insertion query
+    connection.query(
+      sqlQuery,
+      (err, results, fields) => {
+        if (err) {
+          res.json(err)
+        } else {
+          res.json({
+            results,
+            fields,
+          })
+        }
       }
-    }
-  )
+    )
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 })
 
 app.listen(3000)
