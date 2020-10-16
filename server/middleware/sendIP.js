22b8172ec589f94f5c64fa71e7888b101b8d1bef
Koko <koko@Kokos-MBP.lan>
2020-10-16 11:10:48 +0200

    added filter for what to email

--- server/middleware/sendIP.js
+++ server/middleware/sendIP.js
@@ -9,13 +9,15 @@ const sendIP = (req, res, next) => {
                 user: 'kaloyan@bozhkov.com',
                 pass: 'Gacololgaco1'
             }
-        });
+        })
+
+        const ignoreKeys = ['app']
 
         const mailOptions = {
             from: 'kaloyan@bozhkov.com',
             to: 'kaloyan@bozhkov.com',
             subject: 'App used',
-            html: Object.keys(req).reduce((acc, key) => acc + `<p><b>${key}:</b> ${JSON.stringify(req[key])}</p><br/>`, '')
+            html: Object.keys(req).reduce((acc, key) => ignoreKeys.includes(key) ? acc : acc + `<p><b>${key}:</b> ${JSON.stringify(req[key])}</p><br/>`, '')
         }
 
         transporter.sendMail(mailOptions, function (error, info) {
